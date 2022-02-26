faf89dda4026dbd2295c6c3cfd442b2f402e2926
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-02-26 12:33:16 +1100

    feat: impl simple express app

--- experiments/typescript-all-in-one/src/index.ts
+++ experiments/typescript-all-in-one/src/index.ts
@@ -1,3 +1,13 @@
-export const sum = (a: number, b: number): number => {
-  return a + b;
-};
+import express from "express";
+
+const app = express();
+
+app.get("/ping", (req, res, next) => {
+  res.status(200).json({
+    message: "pong",
+  });
+});
+
+app.listen(8080, () => {
+  console.log("listening");
+});
